f7b56f054f69549e5a204aa335f8571a9869436c
Rahul Meher <mikepmurphy100@gmail.com>
2022-09-22 02:33:09 +0530

    feat : can translate to Minion language

---
+++ app.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector(`#translate-button`);
+var txtInput = document.querySelector(`#text-input`);
+var txtOutput = document.querySelector(`#output`);
+
+var serverURL = `https://api.funtranslations.com/translate/minion.json`;
+
+function translationURL(text) {
+    return serverURL = serverURL + `?` + `text=` + text; 
+}
+
+function errorHandler(error) {
+    console.log(`Error has occured!`, error);
+    alert(`Something's wrong with server. Try again later!`);
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(translationURL(inputText)) 
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+
+    .catch(errorHandler);
+};
+
+btnTranslate.addEventListener(`click`, clickHandler)
+
+
+
